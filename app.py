88a49cabda2401a188e396548e0e11763e93a5c2
Justin Huynh <justinhuynh639@gmail.com>
2023-10-04 10:15:31 -0700

    added questions

--- app.py
+++ app.py
@@ -9,5 +9,29 @@ quiz = {
     "question1": {
         "question": "What is the capital of France?"
         "answer": "Paris"
-    }
+    },
+    "question2": {
+        "question": "What is the capital of Germany?"
+        "answer": "Berlin"
+    },
+    "question3": {
+        "question": "What is the capital of Italy?"
+        "answer": "Rome"
+    },
+    "question4": {
+        "question": "What is the capital of Spain?"
+        "answer": "Madrid"
+    },
+    "question5": {
+        "question": "What is the capital of Portugal?"
+        "answer": "Lisbon"
+    },
+    "question6": {
+        "question": "What is the capital of Switzerland?"
+        "answer": "Bern"
+    },
+    "question7": {
+        "question": "What is the capital of Austria?"
+        "answer": "Vienna"
+    },
 }
