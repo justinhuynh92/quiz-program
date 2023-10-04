ca490b03246512406263b30d6ee762112a4c4da0
Justin Huynh <justinhuynh639@gmail.com>
2023-10-04 10:09:10 -0700

    started quiz program

---
+++ app.py
@@ -0,0 +1,13 @@
+# have a variable that tracks the score of the player
+# loop through the dictionary using the key, values pair
+# display each question to the user and allow them to answer
+# tell them if they are right or wrong
+# show the final result when quiz is completed
+
+# add a dictionary that stores questions and answers
+quiz = {
+    "question1": {
+        "question": "What is the capital of France?"
+        "answer": "Paris"
+    }
+}
